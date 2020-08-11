b8d9b0bafc36fd8e45d9f04545a4a0487a844482
Dhruv Rakeshkumar Soni <dsoni20@my.centennialcollege.ca>
2020-08-11 16:54:15 -0400

    Login and Sign Up page

---
+++ SocioCode/Areas/Identity/Data/SocioCodeUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SocioCode.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the SocioCodeUser class
+    public class SocioCodeUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="nvarchar(100)")]
+        public string FristName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string EmailID { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; }
+
+
+    }
+}
---
+++ SocioCode/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SocioCode.Areas.Identity.Data;
+using SocioCode.Models;
+
+[assembly: HostingStartup(typeof(SocioCode.Areas.Identity.IdentityHostingStartup))]
+namespace SocioCode.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SocioCodeContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SocioCodeContextConnection")));
+
+                services.AddDefaultIdentity<SocioCodeUser>(options => options.SignIn.RequireConfirmedEmail = false)
+                    .AddEntityFrameworkStores<SocioCodeContext>();
+            });
+        }
+    }
+}
